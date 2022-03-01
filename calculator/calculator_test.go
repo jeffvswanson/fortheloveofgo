8b4c132740419b7cd2301dc3ad57504091843c4e
jeffvswanson <jeffery.swanson@mckesson.com>
2022-03-01 11:05:03 -0700

    Update tests with test cases

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -3,8 +3,21 @@ package calculator_test
 import (
 	"calculator"
 	"fmt"
+	"math"
 	"testing"
 )
+
+// Tests floating point numbers to ensure parity due to rounding errors
+func withinTolerance(a, b, e float64) bool {
+	if a == b {
+		return true
+	}
+	d := math.Abs(a - b)
+	if b == 0 {
+		return d < e
+	}
+	return (d / math.Abs(b)) < e
+}
 
 func TestAdd(t *testing.T) {
 	tests := []struct {
@@ -19,11 +32,13 @@ func TestAdd(t *testing.T) {
 		{-1, 0, -1},
 		{-1, -1, -2},
 		{0.1, 0.9, 1},
+		{-0.1, -0.9, -1},
+		{-0.1, -0.1, -0.2},
 	}
 	for _, tc := range tests {
 		t.Run(fmt.Sprintf("Add(%v, %v)", tc.a, tc.b), func(t *testing.T) {
 			got := calculator.Add(tc.a, tc.b)
-			if got != tc.want {
+			if !withinTolerance(got, tc.want, 1e-12) {
 				t.Errorf("got %v, want %v", got, tc.want)
 			}
 		})
@@ -31,19 +46,50 @@ func TestAdd(t *testing.T) {
 }
 
 func TestSubtract(t *testing.T) {
-	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	tests := []struct {
+		a    float64
+		b    float64
+		want float64
+	}{
+		{1, 1, 0},
+		{1, 0, 1},
+		{0, 1, -1},
+		{0, 0, 0},
+		{-1, -1, 0},
+		{0.1, 0.1, 0},
+		{-1.1, -0.2, -0.9},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("Subtract(%v. %v)", tc.a, tc.b), func(t *testing.T) {
+			got := calculator.Subtract(tc.a, tc.b)
+			if !withinTolerance(got, tc.want, 1e-12) {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
 	}
 }
 
 func TestMultiply(t *testing.T) {
-	t.Parallel()
-	var want float64 = 20
-	got := calculator.Multiply(5, 4)
-	if got != want {
-		t.Errorf("want %f, got %f", want, got)
+	tests := []struct {
+		a    float64
+		b    float64
+		want float64
+	}{
+		{1, 1, 1},
+		{0, 1, 0},
+		{1, 0, 0},
+		{1, 0.1, 0.1},
+		{-1, 1, -1},
+		{-1, -1, 1},
+		{-1, 0.1, -0.1},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("Multiply(%v, %v)", tc.a, tc.b), func(t *testing.T) {
+			got := calculator.Multiply(tc.a, tc.b)
+			if !withinTolerance(got, tc.want, 1e-12) {
+				t.Errorf("want %f, got %f", tc.want, got)
+			}
+
+		})
 	}
 }
