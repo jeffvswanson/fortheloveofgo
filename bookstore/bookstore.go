ca2cbdfa1186b41c4f6f23f6cb1b1df9ba9e7628
jeffvswanson <jeffery.swanson@mckesson.com>
2022-03-09 11:54:51 -0700

    Add DiscountPercent to Book struct

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -4,22 +4,24 @@ import "fmt"
 
 // Book represents information about a book.
 type Book struct {
-	Title       string
-	Author      string
-	Copies      int
-	Edition     int
-	Price       int // Price in cents
-	SeriesOrder int
+	Title           string
+	Author          string
+	Copies          int
+	Edition         int
+	Price           int // Price in cents
+	SeriesOrder     int
+	DiscountPercent uint // 10 represents a 10% discount
 }
 
 func main() {
 	b := Book{
-		Title:       "For the Love of Go",
-		Author:      "Jeff Swanson",
-		Copies:      1,
-		Edition:     2,
-		Price:       100,
-		SeriesOrder: 1,
+		Title:           "For the Love of Go",
+		Author:          "Jeff Swanson",
+		Copies:          1,
+		Edition:         2,
+		Price:           100,
+		SeriesOrder:     1,
+		DiscountPercent: 0,
 	}
 	fmt.Printf("%+v\n", b)
 }
