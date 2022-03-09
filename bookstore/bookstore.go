36fbf2b2f6663fbe03321a6dad035aaa11874e7f
jeffvswanson <jeffery.swanson@mckesson.com>
2022-03-09 11:49:03 -0700

    Add Book struct

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
+// Book represents information about a book.
+type Book struct {
+	Title   string
+	Author  string
+	Copies  int
+	Edition int
+}
+
 func main() {
-	author := "Jeff"
-	fmt.Println("author:", author)
-	edition := 1
-	fmt.Printf("edition: %d\n", edition)
+	book := Book{
+		Title:   "For the Love of Go",
+		Author:  "Jeff Swanson",
+		Copies:  1,
+		Edition: 2,
+	}
+	fmt.Printf("%+v\n", book)
 }
