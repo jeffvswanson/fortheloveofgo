16143bbddd2dd5b17fbde12c51ec8bbc1b88f77f
jeffvswanson <jeffery.swanson@mckesson.com>
2022-03-15 15:06:27 -0600

    Fix TestAddtoCatalog

--- bookstore/catalog/catalog_test.go
+++ bookstore/catalog/catalog_test.go
@@ -2,9 +2,33 @@ package catalog
 
 import (
 	"bookstore/book"
-	"github.com/google/go-cmp/cmp"
+	"fmt"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
+
+func TestAddToCatalog(t *testing.T) {
+	book1 := book.Book{Title: "First!"}
+	book2 := book.Book{Title: "Segundo"}
+	tests := []struct {
+		c    Catalog
+		b    book.Book
+		want Catalog
+	}{
+		{nil, book1, Catalog{book1}},                   // nil Catalog
+		{make(Catalog, 0), book1, Catalog{book1}},      // Empty catalog
+		{Catalog{book1}, book2, Catalog{book1, book2}}, // Add a Book
+	}
+	for i, tc := range tests {
+		t.Run(fmt.Sprintf("AddToCatalog%d(%v, %+v", i, tc.c, tc.b), func(t *testing.T) {
+			got := tc.c.AddToCatalog(tc.b)
+			if !cmp.Equal(got, tc.want) {
+				t.Errorf("got %+v, want %+v", got, tc.want)
+			}
+		})
+	}
+}
 
 func TestGetAllBooks(t *testing.T) {
 	book0 := book.Book{Title: "Hello There"}
